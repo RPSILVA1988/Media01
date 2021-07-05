372eb1040ea27d8bd4bda9aef08656e628b9990e
RPSILVA1988 <comraniele@hotmail.com>
2021-07-04 21:56:06 -0300

    Desafio media01

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1005_Media1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double A, B, media;
+
+			A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			media = ((A * 3.5) + (B * 7.5)) / 11;
+
+			Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+
+			Console.ReadLine();
+		}
+	}
+}
